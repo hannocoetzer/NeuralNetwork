3325622f1b62633d082e15e93d47c3815aac7b3c
Hanno Coetzer <hcoetzer@sts3d.co.za>
2025-10-30 19:34:39 +0200

    Structure built | next is to iterate through the network

--- neuralnetwork.cpp
+++ neuralnetwork.cpp
@@ -1,74 +1,89 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include <algorithm>
 #include <cmath>
+#include <iostream>
 #include <map>
-#include <set>
 #include <queue>
+#include <set>
 #include <stack>
+#include <string>
+#include <vector>
 using namespace std;
 
 class Node {
 public:
-    float data;
-    Node* next;
-    Node(float value) : data(value), next(nullptr) {}
+  float data;
+  Node *next;
+  Node *prev;
+  Node(float value) : data(value), next(nullptr), prev(nullptr) {}
 };
 
-class NeuralNetwork{
+class NeuralNetwork {
 
-    private:
-        Node* input1;
-        Node* input2;
+private:
+  Node *input1;
+  Node *input2;
 
-    public:
-        NeuralNetwork(){
-            input1 = nullptr;
-            input2 = nullptr;
-        }
+public:
+  NeuralNetwork() {
+    input1 = nullptr;
+    input2 = nullptr;
+  }
 
-        Node* MakeNode()
-        {
-            Node* newNode = new Node(0.5);
-            return newNode;
-        }
+  Node *MakeNode() {
+    Node *newNode = new Node(0.5);
+    return newNode;
+  }
 
-        void JoinNodes(Node* parent,Node* child)
-        {
-           parent->next = child;
-        }
+  void JoinNodes(Node *parent, Node *child) {
+    parent->next = child;
+    child->prev = parent;
+  }
 
-        void builder()
-        {
-            //First level of nodes -- making new nodes
-            Node* lvl1Node1 = MakeNode();
-            Node* lvl1Node2 = MakeNode();
-            Node* lvl1Node3 = MakeNode();
+  void builder() {
+    // First level of nodes -- making new nodes
+    Node *lvl1Node1 = MakeNode();
+    Node *lvl1Node2 = MakeNode();
+    Node *lvl1Node3 = MakeNode();
 
-            //First level -- joing other parent
-            JoinNodes(input1, lvl1Node1);
-            JoinNodes(input1,lvl1Node2);
-            JoinNodes(input1, lvl1Node3);
+    // First level -- joing other parent
+    JoinNodes(input1, lvl1Node1);
+    JoinNodes(input1, lvl1Node2);
+    JoinNodes(input1, lvl1Node3);
 
-            JoinNodes(input2, lvl1Node3);
-            JoinNodes(input2, lvl1Node3);
-            JoinNodes(input2, lvl1Node3);
+    JoinNodes(input2, lvl1Node3);
+    JoinNodes(input2, lvl1Node3);
+    JoinNodes(input2, lvl1Node3);
 
-            //Second level of nodes -- new nodes
-            Node* lvl2Node1 = MakeNode();
-            Node* lvl2Node2 = MakeNode();
+    // Second level of nodes -- new nodes
+    Node *lvl2Node1 = MakeNode();
+    Node *lvl2Node2 = MakeNode();
 
-            //Second level - joing others
-                       
+    // Second level - joing others
+    JoinNodes(lvl1Node1, lvl2Node1);
+    JoinNodes(lvl1Node1, lvl2Node2);
+    JoinNodes(lvl1Node2, lvl2Node1);
+    JoinNodes(lvl1Node2, lvl2Node2);
+    JoinNodes(lvl1Node3, lvl2Node1);
+    JoinNodes(lvl1Node3, lvl2Node2);
 
+    // Output node
+    Node *output = MakeNode();
 
-        }
-
+    // Output - join level2
+    JoinNodes(lvl2Node1, output);
+    JoinNodes(lvl2Node2, output);
+  }
 };
 
 int main() {
-    Node n(10);
-    cout << "Node data: " << n.data << endl;
-    return 0;
+  // Node n(10);
+  // cout << "Node data: " << n.data << endl;
+  //
+  //
+
+  NeuralNetwork nn; // = new NeuralNetwork();
+
+  nn.builder();
+
+  return 0;
 }
